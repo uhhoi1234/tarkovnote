a3b1cea5cde27de569b7be93ba2a5ed773c445b3
uhhoi1234 <89830866+uhhoi1234@users.noreply.github.com>
2023-08-10 15:38:26 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function checkfunction(){
             
-    var koukokujs = document.getElementById('koukoku');
+    var admaxadsjs = document.getElementById('admax-ads');
 
 
 
-    if(koukokujs.Checked == true) {
+    if(admaxadsjs.Checked == true) {
         document.getElementById('honbun').style.display = "block";
         document.getElementById('woods').style.display = "block";
         document.getElementById('customs').style.display = "block";
@@ -15,7 +15,7 @@ function checkfunction(){
         document.getElementById('interchange').style.display = "block";
         document.getElementById('sot').style.display = "block";
 
-        document.getElementById('koukoku').style.display = "none";
+        document.getElementById('admax-ads').style.display = "none";
 
     } else {
         document.getElementById('honbun').style.display = "none";
@@ -28,7 +28,7 @@ function checkfunction(){
         document.getElementById('interchange').style.display = "none";
         document.getElementById('sot').style.display = "none";
 
-        document.getElementById('koukoku').style.display = "block";
+        document.getElementById('admax-ads').style.display = "block";
     }
 
 
